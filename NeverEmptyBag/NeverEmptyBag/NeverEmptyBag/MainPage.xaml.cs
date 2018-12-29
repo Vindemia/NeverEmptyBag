40d7f06062161afacba9ee8004f0ded6880552e4
DIAS Elrik <EDIAS@altimance.fr>
2018-12-29 19:08:50 +0100

    Création du projet

---
+++ NeverEmptyBag/NeverEmptyBag/NeverEmptyBag/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NeverEmptyBag
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
